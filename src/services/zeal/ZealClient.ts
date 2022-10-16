8d5e1d03926f62077984536020cbe2b739964ffd
Robert Collins <58490986+mr-refactor@users.noreply.github.com>
2022-10-16 10:36:40 -0500

    removed combination 'await' and '.then' syntactic sugar from ZealClient for better readability

--- src/services/zeal/ZealClient.ts
+++ src/services/zeal/ZealClient.ts
@@ -35,17 +35,23 @@ export class ZealClient {
   }
 
   private async get(route: string, params: Params.QueryParams): Promise<any> {
-    return await this.#client
-      .get(route, {params})
-      .then((r) => r.data?.data)
-      .catch(this.throwException);
+    try {
+      const r = await this.#client
+        .get(route, { params });
+      return r.data?.data;
+    } catch (err) {
+      return this.throwException(err as AxiosError);
+    }
   }
 
   private async post(route: string, body: Params.Body): Promise<any> {
-    return await this.#client
-      .post(route, body)
-      .then((r) => r.data?.data)
-      .catch(this.throwException);
+    try {
+      const r = await this.#client
+        .post(route, body);
+      return r.data?.data;
+    } catch (err) {
+      return this.throwException(err as AxiosError);
+    }
   }
 
   private throwException(err: AxiosError) {
